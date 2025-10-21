e811d7e507eb1a61061182bded2a2225a158594d
MDeBock <102190577+MDeBock@users.noreply.github.com>
2025-10-20 23:44:02 -0300

    localstorage
    index carga dinamica medicos

--- scripts.js
+++ scripts.js
@@ -42,3 +42,29 @@ if (form) {
 }
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const medicos = JSON.parse(localStorage.getItem('medicos')) || [];
+
+    const contenedor = document.getElementById('tarjetas-container');
+    contenedor.innerHTML = ''; // Limpiar contenido por si hay algo
+
+    medicos.filter(m => m.activo).forEach(medico => {
+        const tarjeta = document.createElement('article');
+        tarjeta.className = 'col-12 col-md-6 col-lg-4 p-3 mt-4';
+        tarjeta.innerHTML = `
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${medico.imagen}" alt="${medico.nombre} ${medico.apellido}" class="img-fluid rounded-pill">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${medico.nombre} ${medico.apellido}</h5>
+                        <p class="card-text">${medico.especialidad}</p>
+                        <p class="card-text"><small class="text-body-secondary">${medico.matricula}</small></p>
+                    </div>
+                </div>
+            </div>
+        `;
+        contenedor.appendChild(tarjeta);
+    });
+});
