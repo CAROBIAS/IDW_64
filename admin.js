7d5d5cab1cf8382b622b7b095506bbf3be4a291a
MDeBock <102190577+MDeBock@users.noreply.github.com>
2025-11-11 02:15:33 -0300

    .

--- admin.js
+++ admin.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const tipoUsuario = sessionStorage.getItem("tipoUsuario");
 
   if (!tipoUsuario || tipoUsuario !== "admin") {
+    document.body.innerHTML = "";
     alert("Acceso denegado. Solo administradores pueden entrar.");
     window.location.href = "index.html";
   } else {
     document.body.style.display = "block";
+    document.documentElement.style.display = "block";
   }
 });
 
